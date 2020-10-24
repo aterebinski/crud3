53e83798919b5af1c272d9084058d9d45d653105
Adam Terebiński <aterebinski@wp.pl>
2020-10-24 11:05:07 +0200

    Adding posts/index. Repairing scss configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
   resources "posts"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
