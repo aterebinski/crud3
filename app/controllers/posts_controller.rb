841cc2499346cad348389acd2144690f0ed50c97
Adam Terebiński <aterebinski@wp.pl>
2020-10-26 12:16:28 +0100

    Edit post. Show post. Delete post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,38 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
-  def edit
-  end
-
   def create
     @post = Post.new(post_params)
     @post.save
     redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def confirm_delete
+    @post = Post.find(params[:id])
   end
 
   private
