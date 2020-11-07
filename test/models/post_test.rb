cf248c3ddf50b9fbd40d7395521ff34cf1cd15e8
Adam Terebiński <aterebinski@wp.pl>
2020-11-07 18:29:43 +0100

    Test model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,9 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "titleize_author" do
+    p1 = Post.create author: "john locke", body: "Welcome!", title: "Welcome!"
+    assert_equal "John Locke", p1.author 
+  end
+
 end
