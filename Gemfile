b9cf068d9ada7e2d7ee341e9091335121b64cc9c
Adam Terebiński <aterebinski@wp.pl>
2020-11-07 23:57:02 +0100

    Test controller

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'turbolinks', '~> 5'
 gem 'turbolinks_render'
 # Rubocop is a Ruby code style checker (linter)
 gem 'rubocop', require: false
+# Allow controllers testing 
+gem 'rails-controller-testing'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,23 +2,37 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get posts_index_url
+    get posts_path
     assert_response :success
+    assert_not_nil assigns(:posts)
   end
 
   test "should get show" do
-    get posts_show_url
+    id = Post.last.id
+    get post_path(id)
     assert_response :success
   end
 
   test "should get new" do
-    get posts_new_url
+    get new_post_path
     assert_response :success
   end
 
   test "should get edit" do
-    get posts_edit_url
+    id=Post.first.id
+    get edit_post_path(id)
     assert_response :success
+  end
+
+  test "should post create" do
+    # posts_before = Post.count
+    # post "/posts", params: {post: {title: "Title", author: "Author", body: "Welcome!"}}
+    # posts_after = Post.count
+    # assert_equal 1, posts_after - posts_before
+    assert_difference('Post.count') do #skrocona wersja powyzszego
+      post posts_path, params: {post: {title: "Title", author: "Author", body: "Welcome!"}}
+    end
+    assert_redirected_to posts_path
   end
 
 end
