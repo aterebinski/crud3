8e694d9cd2ede02b06c52ceb93f2a0e9183e655d
Adam Terebiński <aterebinski@wp.pl>
2020-11-07 17:56:00 +0100

    Model's callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
   validates :body, presence: true
+
+
+  before_create :titleize_author
+
+  private
+
+  def titleize_author
+    self.author = self.author.to_s.titleize
+  end
 end
