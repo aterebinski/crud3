f1aca5c9e3a18af1c73057220db3fd35f0208dd6
Adam Terebiński <aterebinski@wp.pl>
2020-11-14 18:33:51 +0100

    I18n update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     before_action :load_language
 
     def load_language
-        I18n.default_locale = session[:locale] || I18n.default_locale
+        I18n.locale = session[:locale] || I18n.default_locale
     end
 end
